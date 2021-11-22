ebd38f29ff1d11f0fccd7daae8ace20918c8ba99
Royal Krrishna <91977328+RoyalKrrishna@users.noreply.github.com>
2021-11-23 02:11:13 +0530

    Update start.py

--- WebStreamer/bot/plugins/start.py
+++ WebStreamer/bot/plugins/start.py
@@ -2,12 +2,13 @@ from pyrogram import filters, emoji
 from WebStreamer.bot import StreamBot
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
 
-@StreamBot.on_message(filters.command(['start']))
+@StreamBot.on_message(filters.command(['start', 'help']))
 async def start(_, m: Message):
-    await m.reply(f'**Hᴇʏ! {m.from_user.mention(style="md")} 😚\n\nI Aᴍ Fɪʟᴇ Lɪɴᴋ Gᴇɴᴇʀᴀᴛᴇʀ Bᴏᴛ 🤖\n\nI Cᴀɴ Sᴛʀᴇᴀᴍ Yᴏᴜʀ Vɪᴅᴇᴏ Tᴏ! ⏯️\n\nSᴇɴᴅ Aɴʏ Fɪʟᴇ Tᴏ Gᴇᴛ Lɪɴᴋ 🔗**',
+    await m.reply(f'Hi {m.from_user.mention(style="md")}, Send me a file to get an instant stream link.',
                   reply_markup=InlineKeyboardMarkup(
                       [[
                             InlineKeyboardButton(
-                                  f'🔰 Join Our Channel 🔰',
-                                  url='https://github.com/iPopcornMovie')
+                                  f'{emoji.STAR} Source {emoji.STAR}',
+                                  url='https://github.com/EverythingSuckz/TG-FileStreamBot')
                         ]]
+                  ))
